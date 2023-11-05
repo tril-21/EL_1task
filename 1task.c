7540a2f54c32563f33958f034f463d6018c9d53e
tril-21 <ilya.tretyak.1997@list.ru>
2023-11-05 21:08:45 +0700

    Задание 1.4

--- 1task.c
+++ 1task.c
@@ -1,21 +1,44 @@
 #include "stdio.h"
 #include "stdbool.h"
-
-int main(){
-	int a;
-	scanf("%d",&a);
-
+void printNumber(int a){
+	bool s=false;
 	int sizeNumber=8*sizeof(a);
-	int count=0;
 	while(1){
 		if(sizeNumber==0)
 			break;
-		if(a&1){
-			count++;
+		if(a&0x80000000){
+			printf("1");
+			s=true;
+		}else{
+			if(s)
+				printf("0");
 		}
-		a=a>>1;
+		
+		a=a<<1;
 		sizeNumber--;
 	}
-	printf("Количество единиц: %d", count);
+}
+int main(){
+	int a, b, d;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	int c=a;
+	if(a&0x00000004){
+		
+		if(b&0x00000004){
+			c=a;
+		}else{
+		c=a&(~(0x00000004));
+		}
+	}
+	else{
+		c=a|(b&0x00000004);
+	}
+	printf("Двоичная запись 1 числа: ");
+	printNumber(a);
+	printf("\nДвоичная запись 2 числа: ");
+	printNumber(b);
+	printf("\nДвоичная запись числа с заменённым битом: ");
+	printNumber(c);
 	return 0;
 }
