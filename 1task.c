39aa7a3db4f6ca78fe86993fc1714b98fc7d99f4
tril-21 <ilya.tretyak.1997@list.ru>
2023-11-06 18:44:22 +0700

    l

--- 1task.c
+++ 1task.c
@@ -1,6 +1,9 @@
 #include "stdio.h"
 #include "stdbool.h"
-void printNumber(int a){
+
+int main(){
+	int a;
+	scanf("%d",&a);
 	bool s=false;
 	int sizeNumber=8*sizeof(a);
 	while(1){
@@ -17,28 +20,6 @@ void printNumber(int a){
 		a=a<<1;
 		sizeNumber--;
 	}
-}
-int main(){
-	int a, b, d;
-	scanf("%d",&a);
-	scanf("%d",&b);
-	int c=a;
-	if(a&0x00000004){
-		
-		if(b&0x00000004){
-			c=a;
-		}else{
-		c=a&(~(0x00000004));
-		}
-	}
-	else{
-		c=a|(b&0x00000004);
-	}
-	printf("Двоичная запись 1 числа: ");
-	printNumber(a);
-	printf("\nДвоичная запись 2 числа: ");
-	printNumber(b);
-	printf("\nДвоичная запись числа с заменённым битом: ");
-	printNumber(c);
+	
 	return 0;
 }
