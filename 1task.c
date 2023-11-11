fe73e043d457ecc598e0d6f2137fc155f15f1fcd
tril-21 <ilya.tretyak.1997@list.ru>
2023-11-11 20:02:23 +0700

    Задание байта

--- 1task.c
+++ 1task.c
@@ -1,25 +1,10 @@
 #include "stdio.h"
-#include "stdbool.h"
-
 int main(){
-	int a;
+	int a, b, c=0;
 	scanf("%d",&a);
-	bool s=false;
-	int sizeNumber=8*sizeof(a);
-	while(1){
-		if(sizeNumber==0)
-			break;
-		if(a&0x80000000){
-			printf("1");
-			s=true;
-		}else{
-			if(s)
-				printf("0");
-		}
-		
-		a=a<<1;
-		sizeNumber--;
-	}
-	
+	scanf("%d",&b);
+	c=(a&0xFFFFF0FF)|(b&0x00000F00);
+	printf("Вывод результата:\n");
+	printf("1 число:\t0x(a)=%x\n2 число:\t0x(b)=%x\nЧисло с замененным байтом:\t0x(c)=%x\n",a,b,c);
 	return 0;
 }
