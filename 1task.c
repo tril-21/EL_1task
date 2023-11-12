34a9fef56b454550182ea3ac51f563396133ddb9
tril-21 <ilya.tretyak.1997@list.ru>
2023-11-12 12:48:19 +0700

    fixed 4 subtask

--- 1task.c
+++ 1task.c
@@ -1,9 +1,17 @@
 #include "stdio.h"
 int main(){
-	int a, b, c=0;
+	unsigned int a, b, c=0;
 	scanf("%d",&a);
-	scanf("%d",&b);
-	c=(a&0xFFFFF0FF)|(b&0x00000F00);
+	while(1){
+		printf("Введите число от 0 до 255:\t");
+		scanf("%d", &b);
+		if(b>255||b<0){
+			printf("Ошибка\n");
+		}else{
+			break;
+		}
+	}
+	c=(a&0xFF00FFFF)|(b<<16);
 	printf("Вывод результата:\n");
 	printf("1 число:\t0x(a)=%x\n2 число:\t0x(b)=%x\nЧисло с замененным байтом:\t0x(c)=%x\n",a,b,c);
 	return 0;
